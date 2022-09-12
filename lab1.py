51c502169d9e4aff266e442b35eb792420691a7f
Your Name <cersanyx@mail.ru>
2022-09-13 00:52:25 +0300

    4to-to workaet

--- lab1.py
+++ lab1.py
@@ -1,5 +1,6 @@
 #! /home/crsnx/Desktop/t1o1/t101_1/.venv/bin/python3
 import generate_and_evaluate as ge
+from time import time
 
 def executor(rules):
     """Removes conflicting rules.
@@ -19,31 +20,62 @@ def reshala(rules, facts):
     Returns:
         list: knowledge base
     """
-    def _and(rule_list, facts_list):
-    	pass
-    def _not(rule_list, facts_list):
-    	pass
-    def _or(rule_list, facts_list):
-    	pass
-    for rule in rules:
-    	operator = list(rule.get('if'))[0] # забираем логический ключ операции
-    	if operator == 'and':
-    		pass
-    	if operator == 'not':
-    		pass
-    	if operator == 'or':
-    		pass
+    result_list = []
 
+    def _and(rule_list, facts_list):
+    	for rule in rule_list:
+    		checker = False # маркер
+    		for fact in facts_list:
+    			if rule == fact:
+    				checker = True  # нашли - тру
+    		if checker == False:
+    			return False
+    	return True
+
+    def _not(rule_list, facts_list):
+    	for rule in rule_list:
+    		for fact in facts_list:
+    			if rule == fact:
+    				#print(f'{rule}\t{fact}')
+    				return False  # нашли ne - false	
+    	return True
+
+    def _or(rule_list, facts_list):
+    	for rule in rule_list:
+    		for fact in facts_list:
+    			if rule == fact:
+    				return True  # нашли or - true
+    	return False
+    	
+
+    for rule in rules:
+    	#print(rule.get('then'))
+    	operator = list(rule.get('if').keys())[0] # забираем логический ключ операции
+    	rule_list = list(rule.get('if').values())[0] # забираем список значений
+    	if operator == 'and':
+    		if _and(rule_list, facts): # дохуярить list values
+    			result_list.append(rule.get('then'))
+    	if operator == 'not':
+    		if _not(rule_list, facts): # дохуярить list values
+    			result_list.append(rule.get('then'))
+    	if operator == 'or':
+    		if _or(rule_list, facts): # дохуярить list values
+    			result_list.append(rule.get('then'))
+
+    return result_list
 
 
 def main():
-	rules = ge.generate_simple_rules(100,4,10)
-	facts = ge.generate_rand_facts(100,10)
+	time_start = time()
+	rules = ge.generate_simple_rules(100,4,1000)
+	facts = ge.generate_rand_facts(100,100000)
+	reshala(rules,facts)
+	print(f"rules generated in {time()-time_start} seconds")
 	#print(len(rules))
-	print(list(rules[0].get('if').keys())[0])
-	print(rules[0].get('if').values())
-	print(rules[0].get('then'))
-	print(facts)
+	# print(list(rules[0].get('if').keys())[0])
+	# print(list(rules[0].get('if').values())[0])
+	# print(rules[0].get('then'))
+	# print(facts)
 
 if __name__=='__main__':
 	main()
