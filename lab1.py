37b4c0a8cdf2b0eebd73dbf6b3c02c73e551bfb1
Your Name <cersanyx@mail.ru>
2022-09-29 21:25:10 +0300

    Почти финальная версия, Оставить временные засечки и оставить запуск на других функциях генерации

--- lab1.py
+++ lab1.py
@@ -1,81 +1,89 @@
 #! /home/crsnx/Desktop/t1o1/t101_1/.venv/bin/python3
+"""1 лабораторка.
+"""
+
+#from time import time
 import generate_and_evaluate as ge
-from time import time
 
-def executor(rules):
+
+def update_rules(rules):
     """Removes conflicting rules.
-
-    Keyword arguments:
-	rules (list of dict): dictionary of rules
+        Args:
+            rules (dictionary): Словарь правил
+        Returns:
+            List of rules: Список правил(or,and,not)
     """
-    # 1. если условия совпадают на разных/тех же опператорах
-    for rule in reversed(rules):
-        for ind_rule in range(len(rules)-1):
-            if rule['if'].values():
-                pass
-
-
-def reshala(rules, facts):
-    """Summary
-    
-    Args:
-        rules (list of dict): dictionary of rules
-        facts (list): list of facts
-    
-    Returns:
-        list: knowledge base
-    """
-    result_list = []
-
-    def _and(rule_list, facts_list):
-    	for rule in rule_list:
-    		checker = False # маркер
-    		for fact in facts_list:
-    			if rule == fact:
-    				checker = True  # нашли - тру
-    		if checker == False:
-    			return False
-    	return True
-
-    def _not(rule_list, facts_list):
-    	for rule in rule_list:
-    		for fact in facts_list:
-    			if rule == fact:
-    				#print(f'{rule}\t{fact}')
-    				return False  # нашли ne - false	
-    	return True
-
-    def _or(rule_list, facts_list):
-    	for rule in rule_list:
-    		for fact in facts_list:
-    			if rule == fact:
-    				return True  # нашли or - true
-    	return False
-    	
-
+    rules_not = []
+    rules_or = []
+    rules_and = []
     for rule in rules:
-    	#print(rule.get('then'))
-    	rule_list = list(rule.get('if').values())[0] # забираем список значений
-    	if 'and' in rule.get('if'):
-    		if _and(rule_list, facts): # дохуярить list values
-    			result_list.append(rule.get('then'))
-    	if 'not' in rule.get('if'):
-    		if _not(rule_list, facts): # дохуярить list values
-    			result_list.append(rule.get('then'))
-    	if 'or' in rule.get('if'):
-    		if _or(rule_list, facts): # дохуярить list values
-    			result_list.append(rule.get('then'))
+        if rule.get('if').get('and') is not None:
+            rules_and.extend([[rule.get('if').get('and'), rule.get('then')]])
+        if rule.get('if').get('or') is not None:
+            rules_or.extend([[rule.get('if').get('or'), rule.get('then')]])
+        if rule.get('if').get('not') is not None:
+            rules_not.extend([[rule.get('if').get('not'), rule.get('then')]])
+    return [rules_or, rules_and, rules_not]
 
-    return result_list
+def update_facts(facts):
+    """Summary
+        Args:
+            facts (list): Список фактов
+        Returns:
+            dict: Словарь фактов
+    """
+    facts_dict = {}
+    for fact in facts:
+        if facts_dict.get(fact) is None:
+            facts_dict[fact] = True
+    return facts_dict
+
+
+def reshala(rules_list, facts_dict):
+    """Summary
+        Args:
+            rules (list of dict): dictionary of rules
+            facts (list): list of facts
+        Returns:
+            list: knowledge base
+    """
+    for rule in rules_list[0]:
+        #Обработка or
+        for ifor in rule[0]:
+            if facts_dict.get(ifor) is True:
+                if facts_dict.get(rule[1]) is None:
+                    #Чтобы не было одних и тех же, проверяем наличие нового факта
+                    facts_dict[rule[1]] = True
+
+    for rule in rules_list[1]:
+        #Обработка and
+        for ifand in rule[0]:
+            if facts_dict.get(ifand) is None:
+                #Поняли, что нет факта из and-вышли из этого правила
+                break
+            if facts_dict.get(rule[1]) is None:
+                facts_dict[rule[1]] = True
+
+    for rule in rules_list[2]:
+        #Обработка not
+        for ifnot in rule[0]:
+            if facts_dict.get(ifnot) is True:
+                break
+            if facts_dict.get(rule[1]) is None:
+                facts_dict[rule[1]] = True
 
 
 def main():
-	#time_start = time()
-    rules = ge.generate_simple_rules(100,4,10)
-    facts = ge.generate_rand_facts(100,100000)
-    print(sorted(list(rules[0]['if'].values())[0]) == sorted(list(rules[0]['if'].values())[0]) )
-    #print(facts)
-    print(reshala(rules,facts))
-    
-if __name__=='__main__':
-	main()
+    """Main
+    """
+    #time_start = time()
+    rules = ge.generate_simple_rules(100, 4, 10)
+    facts = ge.generate_rand_facts(100, 100)
+    rules_list = update_rules(rules)
+    facts_dict = update_facts(facts)
+    print(len(facts_dict))
+    reshala(rules_list, facts_dict)
+    print(len(facts_dict))
+
+if __name__ == '__main__':
+    main()
